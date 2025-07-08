4b867d02d461958d6261bd80b6a38eca67cb41e5
Samadsust71 <samadsust71@gmail.com>
2025-07-09 03:27:17 +0600

    defined user interface

---
+++ src/app/modules/user/user.interface.ts
@@ -0,0 +1,34 @@
+import { Types } from "mongoose";
+
+export interface IRole {
+  SUPER_ADMIN: "SUPER_ADMIN";
+  ADMIN: "ADMIN";
+  USER: "USER";
+  GUIDE: "GUIDE";
+}
+export enum IsActive {
+  ACTIVE = "ACTIVE",
+  INACTIVE = "INACTIVE",
+  BLOCKED = "BLOCKED",
+}
+
+export interface IAuthProvider {
+  provider: string;
+  providerId: string;
+}
+
+export interface IUser {
+  name: string;
+  email: string;
+  password?: string;
+  phone?: string;
+  picture?: string;
+  address?: string;
+  isDeleted?: string;
+  isActive?: IsActive;
+  isVerified: string;
+  role: IRole;
+  auths: IAuthProvider[];
+  bookings?: Types.ObjectId[];
+  guides?: Types.ObjectId[];
+}
