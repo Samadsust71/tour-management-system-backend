c9a674d4c617d6f642fec5c93f5928144cb4a0c5
Samadsust71 <samadsust71@gmail.com>
2025-07-19 03:37:11 +0600

    configure local strategy for credentials login

--- src/app/modules/auth/auth.service.ts
+++ src/app/modules/auth/auth.service.ts
@@ -1,45 +1,14 @@
 /* eslint-disable @typescript-eslint/no-non-null-assertion */
 import AppError from "../../errorHelpers/AppError";
-import { IUser } from "../user/user.interface";
 import httpStatus from "http-status-codes";
 import bcrypt from "bcryptjs";
 import { User } from "../user/user.model";
-import { createNewAccessTokenWithRefreshToken, createUserTokens } from "../../utils/userTokens";
+import { createNewAccessTokenWithRefreshToken } from "../../utils/userTokens";
 import { JwtPayload } from "jsonwebtoken";
 import { envVars } from "../../config/env";
 
 
-const credentialsLogin = async (payload: Partial<IUser>) => {
-  const { email, password } = payload;
 
-  const isUserExist = await User.findOne({ email });
-  if (!isUserExist) {
-    throw new AppError(
-      httpStatus.BAD_REQUEST,
-      "Email is not registered with us"
-    );
-  }
-
-  const isPasswordMatch = await bcrypt.compare(
-    password as string,
-    isUserExist.password as string
-  );
-    if (!isPasswordMatch) {
-        throw new AppError(httpStatus.UNAUTHORIZED, "Password is incorrect");
-    }
-    
-
-    const userTokens = createUserTokens(isUserExist)
-
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    const {password:pass, ...rest} = isUserExist.toObject();
-
-    return {
-      accessToken: userTokens.accessToken,
-      refreshToken:userTokens.refreshToken,
-      user:rest
-    }
-};
 
 const getNewAccessToken = async (refreshToken:string) => {
    const newAccessToken = await createNewAccessTokenWithRefreshToken(refreshToken)
@@ -61,7 +30,6 @@ const resetPassword = async (oldPassword:string, newPassword:string, decodedToke
 };
 
 export const authServices = {
-  credentialsLogin,
   getNewAccessToken,
   resetPassword
 };
