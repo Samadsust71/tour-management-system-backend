3df7a0f409b30f9100b87ce19fdb939bfc29015d
Samadsust71 <samadsust71@gmail.com>
2025-07-20 01:46:24 +0600

    implemented division routes , controller and services

--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -1,20 +1,25 @@
 import { Router } from "express";
 import { userRoutes } from "../modules/user/user.routes";
 import { authRoutes } from "../modules/auth/auth.routes";
+import { divisionRoutes } from "../modules/division/division.route";
 
-export const router = Router()
+export const router = Router();
 
 const modulesRoutes = [
-    {
-    path:"/user",
-    route: userRoutes
-},
-    {
-    path:"/auth",
-    route: authRoutes
-},
-]
+  {
+    path: "/user",
+    route: userRoutes,
+  },
+  {
+    path: "/auth",
+    route: authRoutes,
+  },
+  {
+    path: "/division",
+    route: divisionRoutes,
+  },
+];
 
-modulesRoutes.forEach(route => {
-    router.use(route.path, route.route)
-})
+modulesRoutes.forEach((route) => {
+  router.use(route.path, route.route);
+});
