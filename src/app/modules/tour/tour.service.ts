d2475cb9751a3ac256108de4e62339e67fabdcbe
Samadsust71 <samadsust71@gmail.com>
2025-07-23 01:40:03 +0600

    Added meta data for tours

--- src/app/modules/tour/tour.service.ts
+++ src/app/modules/tour/tour.service.ts
@@ -20,6 +20,9 @@ const getAllTours = async (query: Record<string, string>) => {
   const filter = { ...query };
   const searchTerm = query.searchTerm || "";
   const sort = query.sort || "-createdAt";
+  const page = Number(query.page) || 1;
+  const limit = Number(query.limit) || 10;
+  const skip = (page - 1) * limit;
 
   for (const field of excludedFields) {
     // eslint-disable-next-line @typescript-eslint/no-dynamic-delete
@@ -30,19 +33,33 @@ const getAllTours = async (query: Record<string, string>) => {
       [field]: { $regex: searchTerm, $options: "i" },
     })),
   };
-  // const fields = query.fields?.split(",").join(" ") || ""
 
   const fields = query.fields?.split(",").join(" ") || "";
-  const tours = await Tour.find(searchQuery)
-    .find(filter)
+  // const tours = await Tour.find(searchQuery)
+  //   .find(filter)
+  //   .sort(sort)
+  //   .select(fields).skip(skip).limit(limit)
+  const filterQuery = Tour.find(filter);
+
+  const tours = filterQuery.find(searchQuery);
+
+  const allTours = await tours
     .sort(sort)
-    .select(fields);
+    .select(fields)
+    .skip(skip)
+    .limit(limit);
   const totalTours = await Tour.countDocuments();
+  const totalPage = Math.ceil(totalTours / limit);
+  const meta = {
+    page: page,
+    limit: limit,
+    total: totalTours,
+    totalPage: totalPage,
+  };
+
   return {
-    data: tours,
-    meta: {
-      total: totalTours,
-    },
+    data: allTours,
+    meta: meta,
   };
 };
 
