e3525f81dc19bf1ebf56da0ec3595085bb97a6c8
Samadsust71 <samadsust71@gmail.com>
2025-07-25 04:40:50 +0600

    part-6 completed

--- src/app/config/env.ts
+++ src/app/config/env.ts
@@ -21,6 +21,17 @@ interface EnvConfig {
   GOOGLE_CALLBACK_URL:string;
   EXPRESS_SESSION_SECRET:string;
   FRONTEND_URL:string;
+  // SSL
+  SSL_STORE_ID:string;
+  SSL_STORE_PASS:string;
+  SSL_PAYMENT_API:string;
+  SSL_VALIDATION_API:string
+  SSL_SUCCESS_BACKEND_URL:string;
+  SSL_FAIL_BACKEND_URL:string;
+  SSL_CANCEL_BACKEND_URL:string;
+  SSL_SUCCESS_FRONTEND_URL:string;
+  SSL_FAIL_FRONTEND_URL:string;
+  SSL_CANCEL_FRONTEND_URL:string;
 }
 
 const isValidNodeEnv = (nodeEnv: string): nodeEnv is NodeEnvironment =>
@@ -43,7 +54,18 @@ const requiredEnvVariables: (keyof EnvConfig)[] = [
   "GOOGLE_CLIENT_SECRET",
   "GOOGLE_CALLBACK_URL",
   "EXPRESS_SESSION_SECRET",
-  "FRONTEND_URL"
+  "FRONTEND_URL",
+  // SSL
+  "SSL_STORE_ID",
+  "SSL_STORE_PASS",
+  "SSL_PAYMENT_API",
+  "SSL_VALIDATION_API",
+  "SSL_SUCCESS_BACKEND_URL",
+  "SSL_FAIL_BACKEND_URL",
+  "SSL_CANCEL_BACKEND_URL",
+  "SSL_SUCCESS_FRONTEND_URL",
+  "SSL_FAIL_FRONTEND_URL",
+  "SSL_CANCEL_FRONTEND_URL",
 ];
 
 const loadEnvVariables = (): EnvConfig => {
--- src/app/modules/booking/booking.service.ts
+++ src/app/modules/booking/booking.service.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import httpStatus from "http-status-codes";
 import AppError from "../../errorHelpers/AppError";
 import { PAYMENT_STATUS } from "../payment/payment.interface";
@@ -6,6 +7,8 @@ import { Tour } from "../tour/tour.model";
 import { User } from "../user/user.model";
 import { BOOKING_STATUS, IBooking } from "./booking.interface";
 import { Booking } from "./booking.model";
+import { ISSLCommerz } from "../sslCommerz/sslCommerz.interface";
+import { SSLService } from "../sslCommerz/sslCommerz.service";
 
 const getTransactionId = () => {
   return `tran_${Date.now()}_${Math.floor(Math.random() * 1000)}`;
@@ -34,11 +37,14 @@ const createBooking = async (payload: Partial<IBooking>, userId: string) => {
     // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
     const amount = Number(tour.costFrom) * Number(payload.guestCount!);
 
-    const booking = await Booking.create([{
+    const booking = await Booking.create(
+      [
+        {
           user: userId,
           status: BOOKING_STATUS.PENDING,
           ...payload,
-        }],
+        },
+      ],
       { session }
     );
 
@@ -67,9 +73,28 @@ const createBooking = async (payload: Partial<IBooking>, userId: string) => {
       .populate("tour", "title costFrom")
       .populate("payment");
 
+    const userAddress = (updatedBooking?.user as any).address;
+    const userEmail = (updatedBooking?.user as any).email;
+    const userPhoneNumber = (updatedBooking?.user as any).phone;
+    const userName = (updatedBooking?.user as any).name;
+
+    const sslPayload: ISSLCommerz = {
+      address: userAddress,
+      email: userEmail,
+      phoneNumber: userPhoneNumber,
+      name: userName,
+      amount: amount,
+      transactionId: transactionId,
+    };
+
+    const sslPayment = await SSLService.sslPaymentInit(sslPayload);
+
     await session.commitTransaction();
     session.endSession();
-    return updatedBooking;
+    return {
+      paymentUrl: sslPayment.GatewayPageURL,
+      booking: updatedBooking,
+    };
   } catch (error) {
     await session.abortTransaction();
     session.endSession();
